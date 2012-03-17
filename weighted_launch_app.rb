1c5f050de9f3cbef410f4a5e9ff2591b38d9218e
Gabe Anzelini <me@gabeanzelini.com>
2012-03-16 20:58:09 -0700

    persists rankings

--- weighted_launch_app.rb
+++ weighted_launch_app.rb
@@ -1,14 +1,29 @@
+require 'yaml'
+
 class WeightedLaunchApp
+  attr_accessor :file_name
   def initialize( apps )
-    @apps = apps.map{ |app| { app => 0 } }.reduce(&:merge)
+    @file_name = File.join Preferences.keymando_directory, ".weighted_launch_app"
+    weights = load_rankings
+    @apps = apps.map{ |app| { app => weights[app.original] || 0 } }.reduce(&:merge)
   end
 
   def apps
     Hash[@apps.to_a.sort_by{ |pair| pair[1] }.reverse].keys
   end
 
+  def load_rankings
+    YAML.load_file @file_name
+  end
+
+  def save_rankings
+    ranking = @apps.map{ |display_item, value| { display_item.original => value } }.reduce( &:merge )
+    File.open( @file_name, 'w' ){ |f| f.puts ranking.to_yaml }
+  end
+
   def run_using( item )
     @apps[item] += 1
+    save_rankings
     LaunchApp.new.run_using( item )
   end
 end
